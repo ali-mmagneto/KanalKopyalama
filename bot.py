5a6f465c6556d1e9bc697eb38c3e3ef902695d18
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-11 12:34:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,4 @@
 from pyrogram import Client, filters
+from config import API_ID, API_HASH, BOT_TOKEN, KANALLAR, KANALIM
+
+Bot = Client("kanalkopy", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
