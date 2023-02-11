f47779693e8a43f09c7106bceed6b85c446c89d2
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-11 12:40:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 APP_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-KANALLAR = list(set(x for x in os.environ.get("KANALLAR", "1276627253").split()))
+KANAL = os.environ.get("KANAL", None)
 KANALIM = os.environ.get("KANALIM", None)
