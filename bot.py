d156338643b69cafeb81e156b89fb0231e123e35
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-11 12:40:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,14 @@
 from pyrogram import Client, filters
-from config import APP_ID, API_HASH, BOT_TOKEN, KANALLAR, KANALIM
+from config import APP_ID, API_HASH, BOT_TOKEN, KANAL, KANALIM
 
 Bot = Client("kanalkopy", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+@Client.on_message(filters.chat(KANAL))
+async def kopy(bot, message):
+    try:
+        await bot.copy_message(
+            chat_id = KANALIM,
+            from_chat_id = KANAL,
+            message_id = message.id)
+    except Exception as e:
+        print(e)
