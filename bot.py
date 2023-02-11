b549af124091f0c829112d75c75849697f43c63a
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-11 13:33:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,11 @@ from pyrogram import Client, filters
 from config import APP_ID, API_HASH, BOT_TOKEN, KANAL, KANALIM
 
 Bot = Client("kanalkopy", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
+owner = "mmagneto"
 @Client.on_message(filters.incoming & filters.channel)
 async def kopy(bot, message):
     try:
+        await bot.send_message(owner, "`Yeni Mesaj Gönderildi`")
         await bot.copy_message(
             chat_id = KANALIM,
             from_chat_id = KANAL,
